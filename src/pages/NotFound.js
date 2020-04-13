42c7de91d246a9ac680072af431fd1b40d7b3a06
Mariana Loaiza <mariloaiza20@gmail.com>
2020-04-12 23:08:58 -0500

    se arregla el titulo

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -9,7 +9,7 @@ class NotFound extends React.Component{
     render(){
         return(
             <div className="container-error">
-                <p className="container-error__title"> 404 Not found </p>
+                <p className="container-error__titulo"> 404 Not found </p>
                 <Link to="/First/Page" className="container-error__btn">Home</Link>
             </div>
         )
